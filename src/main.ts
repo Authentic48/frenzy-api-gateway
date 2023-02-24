8f9ea8d12b77a7b4141ae4df35f1aa7a7717586b
Fall Ibrahim <ibrahim_fall@smartworld.team>
2023-02-24 19:04:59 +0700

    feat: add swagger and helmet

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { INestApplication } from '@nestjs/common';
+import helmet from 'helmet';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +13,32 @@ async function bootstrap() {
   const configService = app.get<ConfigService>(ConfigService);
   const port = parseInt(configService.get('APP_PORT'));
 
+  app.use(
+    helmet({
+      crossOriginResourcePolicy: { policy: 'cross-origin' },
+    }),
+  );
+
+  app.enableCors();
+
+  app.enableShutdownHooks();
+
+  if (process.env.NODE_ENV !== 'prod') {
+    setupOpenApi(app);
+  }
+
   await app.listen(port);
 }
 bootstrap();
+
+function setupOpenApi(app: INestApplication) {
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('TTK - Api documentation')
+    .setDescription('Api description of TinTok')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
+}
